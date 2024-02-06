3c6d8a3c68e70e6767725cd5a673a00db847b37a
iliasabai123 <ilias.abai.alser@gmail.com>
2024-02-07 00:17:33 +0600

    -add main content design

---
+++ src/pages/homePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <HelpBusinessModule/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import HelpBusinessModule from '../modules/HelpBusinessModule.vue'</script>
+
+<style scoped lang="scss">
+
+</style>
