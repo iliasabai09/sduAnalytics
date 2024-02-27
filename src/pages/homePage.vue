7ccc40e9ac2de8e4716b0c04677614c448871b7d
TikTaqTo <adai.02_kz@mail.ru>
2024-02-27 22:47:19 +0600

    OverviewSection
    Добавил блок с Overview, возникли сложности с анимацией, в будущем когда доделаем всю страницу поправлю с ильясом

--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -2,12 +2,15 @@
   <div>
     <HomeSectionModule/>
     <OurServicesModule/>
+    <OverviewModule/>
   </div>
 </template>
 
 <script setup lang="ts">
 import HomeSectionModule from '@/modules/HomeSectionModule.vue'
-import OurServicesModule from '@/modules/OurServicesModule.vue'</script>
+import OurServicesModule from '@/modules/OurServicesModule.vue'
+import OverviewModule from '@/modules/OverviewModule.vue';
+</script>
 
 <style scoped lang="scss">
 
---
+++ src/ui/UOverviewItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="item">{{ text }}</div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  text: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.item {
+  margin-top: 15px;
+  color: var(--titlePrimary);
+  font-size: 16px;
+  font-weight: 600;
+  padding: 13px 15px;
+  border-radius: 5px;
+  border-left: 2px solid var(--primary);
+  background-color: #ffffff;
+  -webkit-box-shadow: -1px 3px 20px 0px rgb(82, 90, 101, 0.1);
+  box-shadow: -1px 3px 20px 0px rgb(82, 90, 101, 0.1);
+  -webkit-transition: 0.4s;
+  
+  
+  /* 
+  Todo: Добавить анимацию
+  background: linear-gradient(to right, black 50%, white 50%);
+  background-size: 200% 100%;
+  background-position: right bottom;
+  transition: all .5s ease-out; */
+}
+</style>
+  
