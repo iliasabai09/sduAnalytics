d61396adb6681b6a34e53d2d00d8638066c9198a
iliasabai123 <ilias.abai.alser@gmail.com>
2024-04-24 22:45:45 +0500

    сделал форму авторизации

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/homePage.vue'
-import PortfolioDetail from '@/pages/portfolioDetail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,10 +10,10 @@ const router = createRouter({
       component: HomePage
     },
     {
-      path: '/portfolioDetail',
-      name: 'portfolioDetail',
-      component: PortfolioDetail
-    },
+      path: '/auth',
+      name: 'auth',
+      component: import('../pages/auth.vue')
+    }
     /*  {
 				path: '/about',
 				name: 'about',
