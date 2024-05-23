1c80ce41b34ad440f933cec3a5378d16bff88fd1
iliasabai123 <ilias.abai.alser@gmail.com>
2024-05-23 05:50:41 +0500

    подправил учителей

--- src/components/TeacherCard.vue
+++ src/components/TeacherCard.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="teacher boxShadow">
     <div class="teacher-img">
-      <img :src="teacher.img || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlJ8PzPLg8d2HJkNytkC-WYHjElSVJ3fHRfw&s'"
+      <img :src="teacher.img || computedImage"
            alt="teacher">
     </div>
-    <div class="teacher-subject bodyMedium">{{ teacher.subjects }}</div>
+    <div class="teacher-subject bodyMedium">{{ teacher.degree }}</div>
     <div class="teacher-title textLarge">{{ teacher.fullName }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
 
-defineProps({
+import { computed } from 'vue'
+
+const props = defineProps({
   teacher: {
     type: Object,
     required: true
   }
+})
+
+const computedImage = computed(() => {
+  return props.teacher.gender === 'male'
+      ? 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6wdqJ1t88ixF2kA3HI-keBoetJMKfIAngUsdR_k24EQ&s'
+      : 'https://www.shareicon.net/data/512x512/2016/07/26/802033_user_512x512.png'
 })
 </script>
 
